76dd7750cefcbadeb436400b66246fd54f459c5e
8bitk9 <67807672+dallisconner@users.noreply.github.com>
2020-10-02 22:26:41 -0400

    Started server

--- server.js
+++ server.js
@@ -2,4 +2,13 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 
+const app = express();
 const port = 7777;
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.listen(port, function() {
+    console.log(`Application now listening on port ${port}.`);
+})
