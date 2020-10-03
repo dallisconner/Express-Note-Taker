b7235e620dbb383c9dc02bce5918b1f01170102b
8bitk9 <67807672+dallisconner@users.noreply.github.com>
2020-10-03 14:02:00 -0400

    Fixed pathing issues

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 
 const app = express();
 const port = 7777;
+const mainDir = path.join(__dirname, "/public");
 
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
@@ -19,4 +20,13 @@ app.get("*", function(req, res) {
 
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(mainDir, "notes.html"));
+});
+
+app.get("/api/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+
+app.get("/api/notes/:id", function(req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    res.json(savedNotes[Number(req.params.id)]);
 });
